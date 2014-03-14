4dc27ab2d871c50a5921944510acf5e55d6494cc
LasalDan <lasaldan@gmail.com>
2014-03-14 11:26:18 -0600

    Undoing Web Edit... Too many ! I think...

--- src/FunGame.java
+++ src/FunGame.java
@@ -4,7 +4,7 @@
 public class FunGame {
 
     public static void main (String[] args) {
-        String msg = "Howdy!!";
+        String msg = "Howdy!";
         for(int i=0; i<5; i++) {
             msg += "!";
             printMsg(msg);
